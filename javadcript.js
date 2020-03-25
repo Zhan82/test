0a838de9c5e7853bed808c32ef85e08b2fef6222
Zhandos <zh.abildinov@gmail.com>
2020-03-25 20:44:52 +0300

    First commit

---
+++ javadcript.js
@@ -0,0 +1,57 @@
+'use dtrict'
+
+
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+console.log(4/0);
+console.log('string'*9)
+let something;
+console.log(something)
+
+let persone = {
+  name: "Bota",
+  age: 38,
+  isMarried: true
+};
+
+console.log(persone["name"]);
+
+let arr = ['Botagoz', 'Zhannur', 'Inzhu','Zhandos'];
+
+console.log(arr[0]);
+
+//alert("Hello world!");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+//let answer = prompt("Есть ли Вам 18?", "");
+
+//console.log(typeof(null));
+
+//console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+    console.log(incr);
+    console.log(decr);
+
+
+    console.log(5%3);
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(!isChecked || isClose);
